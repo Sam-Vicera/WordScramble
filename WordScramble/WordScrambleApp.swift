4882b371ca776420287170cc5e35de044c91e654
Samuel Hernandez Vicera <shernandezvicera@apple.com>
2024-03-07 11:48:48 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Samuel Hernandez Vicera on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
